4a23dc158996576293199c294bd9c707b64e8524
Francisco Lima <francisco.lima@liferay.com>
2023-06-15 19:49:04 -0300

    add first reducer implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,58 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import Header from './components/Header';
+import Error from './components/Error';
+import Loader from './components/Loader';
 import Main from './components/Main';
+import { useReducer } from 'react';
+import StartScreen from './components/StartScreen';
+
+const initialState = {
+	questions: [],
+	status: "loading", // loading, error, ready, active, finished
+}
+
+function reducer(state, action) {
+	switch (action.type) {
+		case "dataReceived":
+			return {
+				...state,
+				questions: action.payload,
+				status: "ready"
+			}
+		case "dataFailed":
+		return {
+			...state,
+			status: "error"
+		}
+		default:
+			throw new Error("Action unknown");
+	}
+}
 
 function App() {
-	const [count, setCount] = useState(0);
+
+	const [{ questions, status }, dispatch] = useReducer(reducer, initialState)
+
+	const numQuestions = questions.length
+
+	useEffect(() => {
+		fetch("http://localhost:8000/questions/")
+		.then((res) => res.json())
+		.then((data) => dispatch({ type: "dataReceived", payload: data }))
+		.catch((err) => dispatch({ type: "dataFailed" }))
+		
+	}, [])
 
 	return (
 		<>
 			<div className="app">
 				<Header />
-				
 				<Main>
-					<p>1/15</p>
-					<p>Questions?</p>
+					{status == "loading" && <Loader />}
+					{status == "error" && <Error />}
+					{status == "ready" && <StartScreen numQuestions={numQuestions}/>}
+					{/* {status == "ready"&& <p>1/15</p>
+					<p>Questions?</p>} */}
 				</Main>
 			</div>
 		</>
