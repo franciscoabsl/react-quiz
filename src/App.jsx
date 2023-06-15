177b5ae0287d1c3aef7119cc5ee1fd2799cae46c
Francisco Lima <francisco.lima@liferay.com>
2023-06-15 20:39:08 -0300

    add answer and points logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,62 +3,82 @@ import Header from './components/Header';
 import Error from './components/Error';
 import Loader from './components/Loader';
 import Main from './components/Main';
-import { useReducer } from 'react';
+import {useReducer} from 'react';
 import StartScreen from './components/StartScreen';
 import Question from './components/Question';
 
 const initialState = {
 	questions: [],
-	status: "loading", // loading, error, ready, active, finished
-	index: 0
-}
+	status: 'loading', // loading, error, ready, active, finished
+	index: 0,
+	answer: null,
+	points: 0,
+};
 
 function reducer(state, action) {
 	switch (action.type) {
-		case "dataReceived":
+		case 'dataReceived':
 			return {
 				...state,
 				questions: action.payload,
-				status: "ready"
-			}
-		case "dataFailed":
+				status: 'ready',
+			};
+		case 'dataFailed':
 			return {
 				...state,
-				status: "error"
-			}
-		case "start":
+				status: 'error',
+			};
+		case 'start':
 			return {
 				...state,
-				status: "active"
-			}
+				status: 'active',
+			};
+		case 'newAnswer':
+			const question = state.questions.at(state.index)
+
+			return {
+				...state,
+				answer: action.payload,
+				points: action.payload === question.correctOption 
+					? state.points + question.points 
+					: state.points
+			};
 		default:
-			throw new Error("Action unknown");
+			throw new Error('Action unknown');
 	}
 }
 
 function App() {
+	const [{questions, status, index, answer}, dispatch] = useReducer(
+		reducer,
+		initialState
+	);
 
-	const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState)
-
-	const numQuestions = questions.length
+	const numQuestions = questions.length;
 
 	useEffect(() => {
-		fetch("http://localhost:8000/questions/")
-		.then((res) => res.json())
-		.then((data) => dispatch({ type: "dataReceived", payload: data }))
-		.catch((err) => dispatch({ type: "dataFailed" }))
-		
-	}, [])
+		fetch('http://localhost:8000/questions/')
+			.then((res) => res.json())
+			.then((data) => dispatch({type: 'dataReceived', payload: data}))
+			.catch((err) => dispatch({type: 'dataFailed'}));
+	}, []);
 
 	return (
 		<>
-			<div className="app">
+			<div className='app'>
 				<Header />
 				<Main>
-					{status == "loading" && <Loader />}
-					{status == "error" && <Error />}
-					{status == "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-					{status == "active" && <Question question={questions[index]}/>}
+					{status == 'loading' && <Loader />}
+					{status == 'error' && <Error />}
+					{status == 'ready' && (
+						<StartScreen
+							numQuestions={numQuestions}
+							dispatch={dispatch}
+						/>
+					)}
+					{status == 'active' && (
+						<Question question={questions[index]} dispatch={dispatch} answer={answer}/>
+					)}
 				</Main>
 			</div>
 		</>
